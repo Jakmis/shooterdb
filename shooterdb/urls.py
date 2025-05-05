ed3e67e2a8aaa254dea7a1872a2c89ebbef9ad5a
jakmis <jakmis007@gmail.com>
2025-05-05 15:00:31 +0200

    pridani klub view

--- shooterdb/urls.py
+++ shooterdb/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.home_view, name="index"),
     path('persons/', views.person_list_view, name='person_list'),
     path('person/<int:person_id>/', views.person_detail_view, name='person_detail'),
+    path("clubs/", views.club_list_view, name="club_list"),
+    path("clubs/<int:club_id>/", views.club_detail_view, name="club_detail"),
     path("api/", api.urls),
 ]
--- shooterdb/views.py
+++ shooterdb/views.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Person, Result
+from .models import Person, Result, Club
 
 # Create your views here.
 
@@ -60,3 +60,11 @@ def person_detail_view(request, person_id):
     }
 
     return render(request, 'person_detail.html', context)
+
+def club_list_view(request):
+    clubs = Club.objects.all()
+    return render(request, 'club_list.html', {'clubs': clubs})
+
+def club_detail_view(request, club_id):
+    club = get_object_or_404(Club, club_id=club_id)
+    return render(request, 'club_detail.html', {'club': club})
